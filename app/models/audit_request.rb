e3aa91b7def1bc2d86819e46c9946590bdc82cc3
Mitosch <mschindowski@gmail.com>
2020-02-11 17:06:11 +0100

    define enum in AuditRequest

--- app/models/audit_request.rb
+++ app/models/audit_request.rb
@@ -1,2 +1,16 @@
 class AuditRequest < ApplicationRecord
+  # Audit types:
+  #
+  # psi   Google PageSpeed Insights
+  #   https://developers.google.com/speed/docs/insights/v5/get-started?hl=en
+  #   * Chrome User Experience Report
+  #   * Lighthouse Report included
+  #
+  # lighthoust  Lighthouse
+  #   https://github.com/GoogleChrome/lighthouse
+  #   * Lighthouse Report only
+  enum audit_type: {
+    psi: 0,
+    lighthouse: 1
+  }
 end
