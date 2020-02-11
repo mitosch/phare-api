0c61d62821c19c8fc6e9bd6dc71904ec5ecdfbb1
Mitosch <mschindowski@gmail.com>
2020-02-11 22:07:22 +0100

    prepare PUT pages to add a page to the queue, do not wrap parameters for json

--- app/controllers/api/v1/public/pages_controller.rb
+++ app/controllers/api/v1/public/pages_controller.rb
@@ -22,8 +22,29 @@ module Api
         #   -d '{"url":"https://www.example.com"}' \
         #   http://localhost:3000/api/v1/pub/pages
         def create_or_update
+          uri = parse_url(page_params[:url])
+
+          page = Page.find_or_create_by(url: uri.to_s)
+          page.save # TODO: change to a datetime field to identify requeue
+          # TODO: page.audit! run PageSpeed API call and save AuditReport
+
           render json: { done: true }
+        rescue URI::InvalidURIError
+          render json: { error: "invalid url" }, status: :bad_request
         end
+
+        private
+          def page_params
+            params.permit([:url])
+          end
+
+          def parse_url(url)
+            unless url.start_with?("http://", "https://")
+              raise URI::InvalidURIError, "Invalid URL", url
+            end
+
+            URI.parse(url)
+          end
       end
     end
   end
