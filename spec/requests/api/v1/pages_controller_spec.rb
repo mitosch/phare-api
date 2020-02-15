bdf71634be5add51a6e645f78a87f26de12f152a
Mitosch <mschindowski@gmail.com>
2020-02-15 15:27:52 +0100

    add test and Swagger api documentation for get page

--- spec/requests/api/v1/pages_controller_spec.rb
+++ spec/requests/api/v1/pages_controller_spec.rb
@@ -1,13 +1,8 @@
 # frozen_string_literal: true
 
-require "rails_helper"
 require "swagger_helper"
 
 RSpec.describe Api::V1::Public::PagesController do
-  before(:all) do
-    FactoryBot.create_list(:page, 5)
-  end
-
   path "/api/v1/pub/pages" do
     get "list pages" do
       produces "application/json"
@@ -24,6 +19,32 @@ RSpec.describe Api::V1::Public::PagesController do
             }
           }
 
+        let!(:pages) { FactoryBot.create_list(:page, 5) }
+        run_test!
+      end
+    end
+  end
+
+  path "/api/v1/pub/pages/{id}" do
+    get "get page" do
+      produces "application/json"
+      parameter name: :id, in: :path, type: :string
+
+      response "200", "page found" do
+        schema type: :object,
+          properties: {
+            id: { type: :integer },
+            url: { type: :string },
+            audit_frequency: { type: :string, enum: ["hourly", "daily"] },
+            status: { type: :string, enum: ["active", "inactive", "archived"] }
+        }
+
+        let(:id) { FactoryBot.create(:page).id }
+        run_test!
+      end
+
+      response "404", "page not found" do
+        let(:id) { "invalid" }
         run_test!
       end
     end
