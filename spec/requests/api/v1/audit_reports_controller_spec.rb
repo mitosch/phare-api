28419fe03552e1a765c732d0f43023adffef278a
Mitosch <mschindowski@gmail.com>
2020-03-15 11:10:11 +0100

    add spec for single audit report

--- spec/requests/api/v1/audit_reports_controller_spec.rb
+++ spec/requests/api/v1/audit_reports_controller_spec.rb
@@ -3,27 +3,115 @@
 require "swagger_helper"
 
 RSpec.describe Api::V1::Public::AuditReportsController do
+  path "/api/v1/pub/pages/{page_id}/audit_reports/{audit_report_id}" do
+    get "get audit report of page" do
+      produces "application/json"
+      parameter name: :page_id, in: :path, type: :string
+      parameter name: :audit_report_id, in: :path, type: :string
+
+      response "200", "audit report found" do
+        schema type: :object,
+          properties: {
+            id: { type: :integer },
+            auditType: { type: :string, enum: ["psi", "lighthouse"] },
+            summary: {
+              type: :object,
+              properties: {
+                fetchTime: { type: :string, format: "date-time" },
+                "max-potential-fid": {
+                  type: :object,
+                  properties: {
+                    id: { type: :string },
+                    score: { type: :number },
+                    numericValue: { type: :number },
+                    displayValue: { type: :string }
+                  }
+                },
+                "first-meaningful-paint": {
+                  type: :object,
+                  properties: {
+                    id: { type: :string },
+                    score: { type: :number },
+                    numericValue: { type: :number },
+                    displayValue: { type: :string }
+                  }
+                },
+                "first-cpu-idle": {
+                  type: :object,
+                  properties: {
+                    id: { type: :string },
+                    score: { type: :number },
+                    numericValue: { type: :number },
+                    displayValue: { type: :string }
+                  }
+                },
+                "first-contentful-paint": {
+                  type: :object,
+                  properties: {
+                    id: { type: :string },
+                    score: { type: :number },
+                    numericValue: { type: :number },
+                    displayValue: { type: :string }
+                  }
+                },
+                "speed-index": {
+                  type: :object,
+                  properties: {
+                    id: { type: :string },
+                    score: { type: :number },
+                    numericValue: { type: :number },
+                    displayValue: { type: :string }
+                  }
+                },
+                "interactive": {
+                  type: :object,
+                  properties: {
+                    id: { type: :string },
+                    score: { type: :number },
+                    numericValue: { type: :number },
+                    displayValue: { type: :string }
+                  }
+                },
+              }
+            },
+            lighthouseResult: {
+              type: :object,
+              properties: {},
+              "x-nullable": true
+            }
+          },
+          required: ["id", "auditType", "summary"]
+
+        let(:page) { create(:page_with_audit_report) }
+        let(:page_id) { page.id }
+        let(:audit_report_id) { page.audit_reports.first.id }
+        run_test!
+      end
+    end
+  end
+
   path "/api/v1/pub/pages/{page_id}/audit_reports" do
     get "list audit reports of page" do
       produces "application/json"
       parameter name: :page_id, in: :path, type: :string
 
-      parameter name: :with,
-                in: :query,
-                type: :array,
-                required: false,
-                collectionFormat: :csv,
-                description: "summary: returns numeric and display values of the key performance metrics, lighthouse: add lighthouse json to the response",
-                items: {
-                  type: :string,
-                  enum: ["summary", "lighthouse"]
-                }
+      # TODO: update to current implementation
+      # parameter name: :with,
+      #           in: :query,
+      #           type: :array,
+      #           required: false,
+      #           collectionFormat: :csv,
+      #           description: "summary: returns numeric and display values of the key performance metrics, lighthouse: add lighthouse json to the response",
+      #           items: {
+      #             type: :string,
+      #             enum: ["summary", "lighthouse"]
+      #           }
 
-      parameter name: :limit,
-                in: :query,
-                type: :integer,
-                required: false,
-                description: "limit number of returned audit reports"
+      # parameter name: :limit,
+      #           in: :query,
+      #           type: :integer,
+      #           required: false,
+      #           description: "limit number of returned audit reports"
 
       response "200", "audit reports found" do
         schema type: :array,
@@ -101,19 +189,9 @@ RSpec.describe Api::V1::Public::AuditReportsController do
             required: ["id", "auditType", "summary"]
           }
 
-        # FactoryBot.create(:page_with_audit_reports)
         let(:page_id) { FactoryBot.create(:page_with_audit_report).id }
-        run_test! do |response|
-          puts "==========>"
-          pp JSON.parse(response.body)
-        end
+        run_test!
       end
-    end
-  end
-
-  # TODO: write FactoryBot first with audit_reports and a full lightspeed response
-  path "/api/v1/pub/pages/{page_id}/audit_reports/{id}" do
-    get "get audit report of page" do
     end
   end
 end
